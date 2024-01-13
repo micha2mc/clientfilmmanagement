907b8049590d68fad27c4453115e7ca80759a42b
micha <micha2mc@gmail.com>
2024-01-13 10:31:32 +0100

    Discovery server

--- src/main/java/com/zakado/zkd/clientfilmmanagement/ClientfilmmanagementApplication.java
+++ src/main/java/com/zakado/zkd/clientfilmmanagement/ClientfilmmanagementApplication.java
@@ -3,8 +3,10 @@ package com.zakado.zkd.clientfilmmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class ClientfilmmanagementApplication {
 
 	public static void main(String[] args) {
